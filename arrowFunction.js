879c8877572b54ec11f7828ce95b8392f7e2d4ee
sagorsenanupam <senanupam10@gmail.com>
2025-06-25 22:32:47 +0600

    edited

--- arrowFunction.js
+++ arrowFunction.js
@@ -3,4 +3,12 @@
 // }
 // add(5, 10);
 
-const add = (a, b) => a + b;
+// const add = (a, b) => a + b;
+// console.log(add(5, 10));
+// For one line
+
+// Now for Multiple lines
+const add = (a, b) => {
+  return a + b;
+};
+console.log(add(5, 10));
