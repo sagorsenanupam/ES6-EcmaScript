365396b974961164b7a150a062dc278ec22296cd
sagorsenanupam <senanupam10@gmail.com>
2025-06-25 22:55:06 +0600

    edited

--- arrowFunction.js
+++ arrowFunction.js
@@ -8,7 +8,14 @@
 // For one line
 
 // Now for Multiple lines
-const add = (a, b) => {
-  return a + b;
-};
-console.log(add(5, 10));
+// const add = (a, b) => {
+//   return a + b;
+// };
+// console.log(add(5, 10));
+
+// Normal arrow function e console.log(arguments) dile koyta arguments eta dibe na
+// normal function e same call korle je koyta arguments ota object akare pass korbe.
+
+// Anonymous function
+document.getElementById("title").addEventListener("click", () => {});
+document.getElementById("title").addEventListener("click", function () {});
