120bb7a0e4dc7026c7c4c7fff589e7d11b9ac2bc
sagorsenanupam <senanupam10@gmail.com>
2025-06-25 21:35:06 +0600

    edited

--- es6.js
+++ es6.js
@@ -1,3 +1,13 @@
 const countryName = "Bangladesh";
 
 let age = 10;
+// we cannot change any value when we use const
+// we ca change the variable value when we use let
+
+const person = {
+  name: "anupam",
+};
+person.name = "sen";
+console.log(person);
+
+// in here we are changing a properties value not the keys
