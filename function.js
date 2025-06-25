e4d4df1c5e14d915d77017997f2bb1e5ed8bcc9c
sagorsenanupam <senanupam10@gmail.com>
2025-06-25 21:50:24 +0600

    edited

---
+++ function.js
@@ -0,0 +1,8 @@
+function sum(num1 = 0, num2 = 0) {
+  const result = num1 + num2;
+  console.log(result);
+}
+sum(10);
+
+// console.log(sum.length);
+//ekhane length koto gula parameter nisi seta print kortese
