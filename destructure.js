b78438256e402876f3c48dd065d9c8a9407fb21b
sagorsenanupam <senanupam10@gmail.com>
2025-07-01 20:29:41 +0600

    edited

--- destructure.js
+++ destructure.js
@@ -1,14 +1,18 @@
-const hena = {
-  name: "Hena",
-  age: 10,
-  friends: ["sakib", "ferdous", "bappa"],
-  status: "Not Found",
-};
+// const hena = {
+//   name: "Hena",
+//   age: 10,
+//   friends: ["sakib", "ferdous", "bappa"],
+//   status: "Not Found",
+// };
 // const henaStatus = hena.status;
 // const henaName = hena.name;
 // const henaAge = hena.age;
 // const henaFriends = hena.friends;
 // console.log(henaStatus);
 
-const { name, age, status, friends } = hena;
-console.log(name);
+// const { name, age, status, friends } = hena;
+// console.log(name);
+
+// const person = ["Alom", "Balom", "Chalom", { name: "anu" }];
+// const [bolod, o, p, obj] = person;
+// console.log(obj);
