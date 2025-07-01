e1878ad1d7f98160d67b852b1f7da3b3005dc6e4
sagorsenanupam <senanupam10@gmail.com>
2025-07-01 21:31:09 +0600

    edited

--- object.js
+++ object.js
@@ -7,9 +7,18 @@ const person = {
 };
 // Notun kore property add korte parbo na baire theke object.seal() use korle
 
-Object.seal(person);
-Object.freeze(person);
+// Object.seal(person);
+// Object.freeze(person);
 
 // console.log(Object.keys(person));
 // console.log(Object.values(person));
 // console.log(Object.entries(person));
+
+// for (let key of Object.keys(person)) {
+//   console.log(`Key hocche: ${key},
+// Value hocche: ${person[key]}`);
+// }
+
+for (let [key, value] of Object.entries(person)) {
+  console.log(key, value);
+}
